3d012cc625c11ccd907e5490b647f51111b6b7bf
hyeonwookim <wooin.dev@gmail.com>
2023-10-09 23:46:29 +0900

    [init] recruit CRUD API 생성
    -기본 예외처리 NotFoundException 설정
    -Swagger 적용

---
+++ src/main/java/com/wooin/wantedinternship/recruit/controller/RecruitController.java
@@ -0,0 +1,74 @@
+package com.wooin.wantedinternship.recruit.controller;
+
+
+import com.wooin.wantedinternship.common.dto.ApiResponseDto;
+import com.wooin.wantedinternship.recruit.dto.RecruitRequestDto;
+import com.wooin.wantedinternship.recruit.dto.RecruitResponseDto;
+import com.wooin.wantedinternship.recruit.service.RecruitService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@Tag(name = "채용공고 API", description = "채용공고 생성, 조회, 수정, 삭제 기능과 검색기능.")
+public class RecruitController {
+    //멤버 선언
+    private final RecruitService recruitService;
+
+    //생성자
+    public RecruitController(RecruitService recruitService) {
+        this.recruitService = recruitService;
+    }
+
+    //메소드
+    @Operation(summary = "채용공고 목록 조회")
+    @GetMapping("/recruitments")
+    public ResponseEntity<List<RecruitResponseDto>> selectRecruitList() {
+
+        List<RecruitResponseDto> recruitResponseDtos = recruitService.selectRecruitList();
+        return new ResponseEntity<>(recruitResponseDtos, HttpStatus.OK);
+    }
+
+    @Operation(summary = "채용공고 단건 조회")
+    @GetMapping("/recruitments/{recruitId}")
+    public ResponseEntity<RecruitResponseDto> selectRecruitOne(
+            @PathVariable("recruitId") Long recruitId
+    ) {
+        RecruitResponseDto responseDto = recruitService.selectRecruitOne(recruitId);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
+
+    @Operation(summary = "채용공고 생성")
+    @PostMapping("/recruitments")
+    public ResponseEntity<RecruitResponseDto> createRecruit(
+            @RequestBody RecruitRequestDto requestDto,
+            Long companyId //todo 이후에 회사정보를 유저정보에서 가져오는 식으로 변경 필요
+    ) {
+        RecruitResponseDto responseDto = recruitService.createRecruit(requestDto, companyId);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
+
+    @Operation(summary = "채용공고 수정", description = "모든 채용공고 정보를 포함하여 요청해야 한다. 채용공고 수정 기능")
+    @PutMapping("/recruitments/{recruitId}")
+    public ResponseEntity<RecruitResponseDto> modifyRecruit(
+            @RequestBody RecruitRequestDto requestDto,
+            @PathVariable("recruitId") Long recruitId
+    ) {
+        RecruitResponseDto responseDto = recruitService.modifyRecruit(requestDto, recruitId);
+        return new ResponseEntity<>(responseDto, HttpStatus.ACCEPTED);
+    }
+
+    @Operation(summary = "채용공고 삭제")
+    @DeleteMapping("/recruitments/{recruitId}")
+    public ResponseEntity<ApiResponseDto> deleteRecruit(
+            @PathVariable Long recruitId
+    ) {
+        recruitService.deleteRecruit(recruitId);
+        return new ResponseEntity<>(new ApiResponseDto(HttpStatus.OK.value(), recruitId+"번의 채용공고가 삭제되었습니다."),HttpStatus.OK);
+    }
+}
--- src/main/java/com/wooin/wantedinternship/recruit/entity/Recruit.java
+++ src/main/java/com/wooin/wantedinternship/recruit/entity/Recruit.java
@@ -2,6 +2,7 @@ package com.wooin.wantedinternship.recruit.entity;
 
 import com.wooin.wantedinternship.common.entity.Timestamped;
 import com.wooin.wantedinternship.company.entity.Company;
+import com.wooin.wantedinternship.recruit.dto.RecruitRequestDto;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -41,5 +42,24 @@ public class Recruit extends Timestamped {
         this.contents = contents;
         this.skill = skill;
         this.company = company;
+    }
+
+    public Recruit(RecruitRequestDto requestDto, Company company) {
+        this.position = requestDto.getPosition();
+        this.reward = requestDto.getReward();
+        this.contents = requestDto.getContents();
+        this.skill = requestDto.getSkill();
+        this.company = company;
+    }
+
+
+    //메소드
+    public Recruit modifyRecruit(RecruitRequestDto requestDto) {
+        this.position = requestDto.getPosition();
+        this.reward = requestDto.getReward();
+        this.contents = requestDto.getContents();
+        this.skill = requestDto.getSkill();
+
+        return this;
     }
 }
